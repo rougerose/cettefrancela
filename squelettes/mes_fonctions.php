05e42ca04d77b0666489d8c69c4e23a6bdebed18
christophe ld <christophe@rouge-rose.net>
2010-03-26 13:37:48 +0100

    slider : début - les bases - on est sur la rubrique 230 (à modifier à nouveau en production)

--- squelettes/mes_fonctions.php
+++ squelettes/mes_fonctions.php
@@ -2,7 +2,7 @@
 // restrictions d'affichage des champs extra relatifs aux volumes
 // voir http://www.spip-contrib.net/Champs-Extras-2
 include_spip('inc/cextras_autoriser');
-// restreindre les champs 'isbn, prix, disponiblite, presentation' des articles, sur les rubriques 194, 229 (volumes 1 et 2)
-restreindre_extras('article', array('isbn','disponibilite','presentation','prix'), array(194,229));
+// restreindre les champs 'isbn, prix, disponiblite, presentation' des articles, sur les rubriques 194, 230 (volumes 1 et 2)
+restreindre_extras('article', array('isbn','disponibilite','presentation','prix'), array(194,230));
 
 ?>
