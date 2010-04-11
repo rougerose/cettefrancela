312463b01f34ea0be935f82293b745bf860e29b6
christophe ld <christophe@rouge-rose.net>
2010-04-11 20:22:50 +0200

    actus dans le sommaire et mise en avant des deux volumes

--- squelettes/mes_fonctions.php
+++ squelettes/mes_fonctions.php
@@ -5,4 +5,23 @@ include_spip('inc/cextras_autoriser');
 // restreindre les champs 'isbn, prix, disponiblite, presentation' des articles, sur les rubriques 194, 230 (volumes 1 et 2)
 restreindre_extras('article', array('isbn','disponibilite','presentation','prix'), array(194,230));
 
+
+function nom_mois_court($numdate){
+        $tableau_mois = array (
+        "01"=>"janv.",
+        "02"=>"f&eacute;v.",
+        "03"=>"mar.",
+        "04"=>"avr.",
+        "05"=>"mai",
+        "06"=>"juin",
+        "07"=>"juil.",
+        "08"=>"ao&ucirc;t",
+        "09"=>"sept.",
+        "10"=>"oct.",
+        "11"=>"nov.",
+        "12"=>"d&eacute;c."
+        );
+        return $tableau_mois[affdate_base($numdate, 'mois')];
+}
+
 ?>
